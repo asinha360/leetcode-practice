f8dba6e3d819a8d048c10379b55ce6efbaffff5b
sniperzero360 <94192552+Sniperzero360@users.noreply.github.com>
2024-08-06 13:34:48 -0400

    feat: Create new problem files
    - Created 3016-minimum-number-of-pushes-to-type-word-II.py in daily-problems
    - Created 70-climbing-stairs.py in blind-75

---
+++ blind-75/70-climbing-stairs.py
@@ -0,0 +1,53 @@
+"""
+70. Climbing Stairs
+
+Level: Easy
+
+You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+
+Example 2:
+
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+
+ 
+
+Constraints:
+
+    1 <= n <= 45
+"""
+
+"""
+Approach: Dynamic Programming distillation from DFS binary tree in O(2^n) down to O(n)
+"""
+
+#   NeetCode's explanation for this rocks!! - https://www.youtube.com/watch?v=Y0lT9Fck7qI&t=7s
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        
+        one, two = 1, 1
+
+        for i in range(n - 1):
+            
+            temp = one
+            one = one + two
+            two = temp
+
+        return one
