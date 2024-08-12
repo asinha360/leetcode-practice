9bbf0e2baf41b02a19341d51bbfd7090c2bfbfc0
sniperzero360 <94192552+Sniperzero360@users.noreply.github.com>
2024-08-12 17:23:51 -0400

    feat: Create new problem file
    - Created 703-kth-largest-element-in-a-stream.py in daily-practice

---
+++ daily-practice/703-kth-largest-element-in-a-stream.py
@@ -0,0 +1,65 @@
+"""
+703. Kth Largest Element in a Stream
+
+Level: Easy
+
+Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Implement KthLargest class:
+
+    KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of integers nums.
+    int add(int val) Appends the integer val to the stream and returns the element representing the kth largest element in the stream.
+
+ 
+
+Example 1:
+
+Input
+["KthLargest", "add", "add", "add", "add", "add"]
+[[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
+Output
+[null, 4, 5, 5, 8, 8]
+
+Explanation
+KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]);
+kthLargest.add(3);   // return 4
+kthLargest.add(5);   // return 5
+kthLargest.add(10);  // return 5
+kthLargest.add(9);   // return 8
+kthLargest.add(4);   // return 8
+
+ 
+
+Constraints:
+
+    1 <= k <= 104
+    0 <= nums.length <= 104
+    -104 <= nums[i] <= 104
+    -104 <= val <= 104
+"""
+import heapq
+
+class KthLargest:
+
+    def __init__(self, k: int, nums: list[int]):
+        
+        self.minHeap, self.k = nums, k
+
+        heapq.heapify(self.minHeap)
+
+        while len(self.minHeap) > k:
+            heapq.heappop(self.minHeap)
+
+    def add(self, val: int) -> int:
+        
+        heapq.heappush(self.minHeap, val)
+
+        if len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
+        
+        return self.minHeap[0]
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
